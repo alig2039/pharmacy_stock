bb080815313541e8ceee88c04e494929f454cb0f
alig2039 <alig2039@yahoo.com>
2022-11-25 16:44:21 +0300

    djangostaticfiles removed from installed apps setting

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    #"django.contrib.staticfiles",
     "django_extensions",
     "stock_manager",
     "django_tables2",
