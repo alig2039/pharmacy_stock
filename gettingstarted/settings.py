8d391ff208085eb7aa8d702a2c5834f456dd634a
alig2039 <alig2039@yahoo.com>
2022-11-25 08:46:35 +0300

    Logging config changed for additional detail

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -158,4 +158,20 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 django_heroku.settings(locals())
