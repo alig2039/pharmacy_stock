77caeebc31c626e890f2c5c4f5f75eafaa3da786
alig2039 <alig2039@yahoo.com>
2022-11-25 10:29:52 +0300

    changed static loading in base html files

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 admin.autodiscover()
 
@@ -49,6 +51,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    urlpatterns += staticfiles_urlpatterns()
 
 handler403 = 'stock_manager.views.permission_denied'
