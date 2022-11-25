9c6f5ab64389b89b869708bab04aecb02a8eab1b
alig2039 <alig2039@yahoo.com>
2022-11-25 09:24:42 +0300

    static url setting changed

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -146,9 +146,11 @@ USE_TZ = True
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
