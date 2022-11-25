de27c33eb26e3667d173c3cf259d11062d99da78
alig2039 <alig2039@yahoo.com>
2022-11-25 08:55:59 +0300

    whitenoise configurations changed

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
