16ff2b2d39eb792ddade82c5a323cdf53c5c415e
alig2039 <alig2039@yahoo.com>
2022-11-25 16:30:37 +0300

    staticfilesstoragesetting changed

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -157,7 +157,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
