1764966684fb9829f5711c906d795b0019cfd710
alig2039 <alig2039@yahoo.com>
2022-11-25 09:39:19 +0300

    django secret changed

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
+SECRET_KEY = "deb579d5-70ab-4921-a6b5-8d6ce49fc23a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
